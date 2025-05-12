30e2acee3feb8158a41d73534c089c1f22822e6e
SouhaylaOchen <SOUHAYLA.OUCHEN.ESTN24@ump.ac.ma>
2025-05-12 17:31:39 +0100

    Update vetService.js

--- frontend/src/services/vetService.js
+++ frontend/src/services/vetService.js
@@ -1,20 +1,17 @@
 const BASE_URL = "http://localhost:5000/api/vets";
 
-// جلب جميع الأطباء البيطريين
 export const getAllVets = async () => {
   const response = await fetch(BASE_URL);
   if (!response.ok) throw new Error("Failed to fetch vets");
   return await response.json();
 };
 
-// جلب طبيب واحد
 export const getVetById = async (id) => {
   const response = await fetch(`${BASE_URL}/${id}`);
   if (!response.ok) throw new Error("Failed to fetch vet");
   return await response.json();
 };
 
-// إنشاء طبيب جديد
 export const createVet = async (vetData) => {
   const response = await fetch(BASE_URL, {
     method: "POST",
